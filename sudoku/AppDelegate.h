e24110b77e8dda7c732d22114a899ad94795374e
Andrew Boryk <aboryk@mercymavericks.edu>
2015-05-10 18:05:06 -0400

    Initial Commit

---
+++ sudoku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sudoku
+//
+//  Created by Andrew Boryk on 5/10/15.
+//  Copyright (c) 2015 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sudoku/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sudoku
+//
+//  Created by Andrew Boryk on 5/10/15.
+//  Copyright (c) 2015 Andrew Boryk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
